3d7e3881d5976dea151f318c730fa740c07538b6
Daniil Paveliev <khiromuelrick@gmail.com>
2023-05-15 00:10:31 +0300

    Init commit

---
+++ src/main/java/ru/theboys/deliverypointratinggateway/DeliveryPointRatingGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.theboys.deliverypointratinggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPointRatingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryPointRatingGatewayApplication.class, args);
+    }
+
+}
